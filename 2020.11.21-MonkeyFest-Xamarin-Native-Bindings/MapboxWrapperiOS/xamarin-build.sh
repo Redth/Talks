039fb0969548e49c15e6c4ce6a27f501af62f35a
Jonathan Dick <jondick@gmail.com>
2020-11-21 10:45:39 -0500

    Add lipo info after building binary

--- 2020.11.21-MonkeyFest-Xamarin-Native-Bindings/MapboxWrapperiOS/xamarin-build.sh
+++ 2020.11.21-MonkeyFest-Xamarin-Native-Bindings/MapboxWrapperiOS/xamarin-build.sh
@@ -3,3 +3,5 @@ xcodebuild -sdk iphonesimulator -arch x86_64
 
 lipo -create -o build/Release-iphoneos/MapboxWrapper.framework/MapboxWrapper \
     build/Release-iphonesimulator/MapboxWrapper.framework/MapboxWrapper
+
+lipo -info build/Release-iphoneos/MapboxWrapper.framework/MapboxWrapper
